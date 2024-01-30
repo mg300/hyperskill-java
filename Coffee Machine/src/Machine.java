a5971223bcf1c914ab2d0f094babce2842a68309
Mateusz G <89965510+MateuszG2000@users.noreply.github.com>
2024-01-30 13:35:42 +0100

    stage 2 implemented

--- Coffee Machine/src/Machine.java
+++ Coffee Machine/src/Machine.java
@@ -4,21 +4,17 @@ import java.util.function.Consumer;
 public class Machine {
     final private Scanner scanner;
     final private Consumer<String> output;
+    private int cups;
     public Machine(Scanner scanner, Consumer<String> output){
         this.scanner = scanner;
         this.output=output;
         powerOn();
     }
     public void powerOn(){
+        output.accept("Write how many cups of coffee you will need: ");
+        cups = scanner.nextInt();
+        output.accept("For 25 cups of coffee you will need: \n"+cups*200 +" ml of water\n"+cups*50 +" ml of milk\n"+cups*15 +" g of coffe beans\n");
 
-        output.accept("""
-                Starting to make a coffee
-                Grinding coffee beans
-                Boiling water
-                Mixing boiled water with crushed coffee beans
-                Pouring coffee into the cup
-                Pouring some milk into the cup
-                Coffee is ready!""");
     }
 
 }
