4d96dc379b9060c14df1ce438d2292237e16f2a9
Mateusz G <89965510+MateuszG2000@users.noreply.github.com>
2024-02-16 12:48:36 +0100

    Zookeeper stage 1/4

--- Zookeeper/src/Main.java
+++ Zookeeper/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("I love animals!\n" +
+                "Let's check on the animals...\n" +
+                "The deer looks fine.\n" +
+                "The bat looks happy.\n" +
+                "The lion looks healthy.");
     }
 }
