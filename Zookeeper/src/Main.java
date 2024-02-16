d2430d3fdef6da286e2ad9079b84d3fb70d85631
Mateusz G <89965510+MateuszG2000@users.noreply.github.com>
2024-02-16 12:56:10 +0100

    Zookeeper stage 2/4

--- Zookeeper/src/Main.java
+++ Zookeeper/src/Main.java
@@ -5,5 +5,27 @@ public class Main {
                 "The deer looks fine.\n" +
                 "The bat looks happy.\n" +
                 "The lion looks healthy.");
+
+    String camel = """
+            Switching on the camera in the camel habitat...
+             ___.-''''-.
+            /___  @    |
+            ',,,,.     |         _.'''''''._
+                 '     |        /           \\
+                 |     \\    _.-'             \\
+                 |      '.-'                  '-.
+                 |                               ',
+                 |                                '',
+                  ',,-,                           ':;
+                       ',,| ;,,                 ,' ;;
+                          ! ; !'',,,',',,,,'!  ;   ;:
+                         : ;  ! !       ! ! ;  ;   :;
+                         ; ;   ! !      ! !  ; ;   ;,
+                        ; ;    ! !     ! !   ; ;
+                        ; ;    ! !    ! !     ; ;
+                       ;,,      !,!   !,!     ;,;
+                       /_I      L_I   L_I     /_I
+            Look at that! Our little camel is sunbathing!""";
+    System.out.println(camel);
     }
 }
