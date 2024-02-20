f395c85a7f40cb1c14b60a47b00d88693a0b4c07
Mateusz G <89965510+MateuszG2000@users.noreply.github.com>
2024-02-20 15:27:35 +0100

    Last pencil stage 2/5

--- Last pencil/src/Main.java
+++ Last pencil/src/Main.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("||||||||");
-        System.out.println("Your turn!");
-        System.out.println("|||");
-        System.out.println("Your turn!");
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many pencils would you like to use:");
+        int pencilsQuantity = scanner.nextInt();
+        System.out.println("Who will be the first (John, Jack):");
+        scanner.nextLine();
+        String winner = scanner.nextLine();
+        System.out.println("|".repeat(pencilsQuantity));
+        System.out.println(winner+" is going first!");
     }
 }
