61a81ebdde7d205dd7257ad1b0f7c50e94278866
Mateusz G <89965510+MateuszG2000@users.noreply.github.com>
2024-02-20 15:45:29 +0100

    Last pencil stage 3/5

--- Last pencil/src/Main.java
+++ Last pencil/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -6,9 +7,32 @@ public class Main {
         System.out.println("How many pencils would you like to use:");
         int pencilsQuantity = scanner.nextInt();
         System.out.println("Who will be the first (John, Jack):");
+        String[] players = {"John", "Jack"};
         scanner.nextLine();
-        String winner = scanner.nextLine();
-        System.out.println("|".repeat(pencilsQuantity));
-        System.out.println(winner+" is going first!");
+        String firstPlayerName;
+        int firstPlayerIndex;
+        while(true){
+            firstPlayerName = scanner.nextLine();
+            firstPlayerName=firstPlayerName.substring(0, 1).toUpperCase() + firstPlayerName.substring(1);
+            firstPlayerIndex= Arrays.binarySearch(players, firstPlayerName);
+            if(firstPlayerIndex<0) {
+                System.out.println("Name is not allowed");
+                continue;
+            };
+            break;
+        }
+
+            while(pencilsQuantity>0){
+                System.out.println("|".repeat(pencilsQuantity));
+                System.out.println(players[firstPlayerIndex]+"'s turn:");
+                int pencils = scanner.nextInt();
+                if(firstPlayerIndex==0){
+                    firstPlayerIndex=1;
+                }else {
+                    firstPlayerIndex=0;
+                }
+                pencilsQuantity-=pencils;
+        }
+
     }
 }
