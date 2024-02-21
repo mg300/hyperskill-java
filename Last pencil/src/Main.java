2ee11f9b1fe2c66f27020ac15ff56da0566405e3
Mateusz G <89965510+MateuszG2000@users.noreply.github.com>
2024-02-21 09:57:02 +0100

    Last pencil stage 4/5

--- Last pencil/src/Main.java
+++ Last pencil/src/Main.java
@@ -1,38 +1,21 @@
-import java.util.Arrays;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("How many pencils would you like to use:");
-        int pencilsQuantity = scanner.nextInt();
-        System.out.println("Who will be the first (John, Jack):");
+        Reader reader = new Reader();
         String[] players = {"John", "Jack"};
-        scanner.nextLine();
-        String firstPlayerName;
-        int firstPlayerIndex;
-        while(true){
-            firstPlayerName = scanner.nextLine();
-            firstPlayerName=firstPlayerName.substring(0, 1).toUpperCase() + firstPlayerName.substring(1);
-            firstPlayerIndex= Arrays.binarySearch(players, firstPlayerName);
-            if(firstPlayerIndex<0) {
-                System.out.println("Name is not allowed");
-                continue;
-            };
-            break;
-        }
 
+        System.out.println("How many pencils would you like to use:");
+        int pencilsQuantity = reader.readNumeric();
+        System.out.println("Who will be the first (John, Jack):");
+
+        int currentPlayer = reader.getPlayerIndex(players);
             while(pencilsQuantity>0){
                 System.out.println("|".repeat(pencilsQuantity));
-                System.out.println(players[firstPlayerIndex]+"'s turn:");
-                int pencils = scanner.nextInt();
-                if(firstPlayerIndex==0){
-                    firstPlayerIndex=1;
-                }else {
-                    firstPlayerIndex=0;
-                }
+                System.out.println(players[currentPlayer]+"'s turn:");
+                int pencils = reader.readPencils(pencilsQuantity);
                 pencilsQuantity-=pencils;
+                currentPlayer = (currentPlayer == 0) ? 1 : 0;
         }
+            System.out.println(players[currentPlayer]+" won");
 
     }
 }
---
+++ Last pencil/src/Reader.java
@@ -0,0 +1,56 @@
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Reader {
+    final private Scanner scanner;
+    public Reader(){
+        scanner = new Scanner(System.in);
+    }
+    public int getPlayerIndex(String[] players){
+        int playerIndex;
+          while(true){
+              String name= scanner.nextLine();
+              name=name.substring(0, 1).toUpperCase() + name.substring(1);
+              playerIndex= Arrays.binarySearch(players, name);
+              if(playerIndex<0) {
+                  System.out.println("Choose between 'John' and 'Jack'");
+                  continue;
+              };
+              break;
+      }
+        return playerIndex;
+    }
+    public int readPencils(int pencilsQuantity){
+        int pencils;
+        while (true) {
+            pencils = scanner.nextInt();
+            if (pencils > pencilsQuantity) {
+                System.out.println("Too many pencils were taken");
+            } else if (pencils < 1 || pencils > 3) {
+                System.out.println("Possible values: '1', '2' or '3'");
+            } else {
+                break;
+            }
+        }
+        return pencils;
+    }
+    public int readNumeric(){
+        int pencilsQuantity;
+        while(true){
+            try{
+                pencilsQuantity = scanner.nextInt();
+                scanner.nextLine();
+                if(pencilsQuantity<=0){
+                    System.out.println("The number of pencils should be positive");
+                }else{
+                    break;
+                }
+            }catch(InputMismatchException e){
+                System.out.println("The number of pencils should be numeric");
+                scanner.nextLine();
+            }}
+
+        return pencilsQuantity;
+    }
+}
