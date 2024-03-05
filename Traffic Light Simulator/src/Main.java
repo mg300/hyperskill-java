754bd08301eee1ad497c9e0a8b74e8a15d9907c7
Mateusz G <89965510+MateuszG2000@users.noreply.github.com>
2024-03-05 19:55:17 +0100

    stage 2/6

--- Traffic Light Simulator/src/Main.java
+++ Traffic Light Simulator/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -10,6 +11,36 @@ public class Main {
         System.out.println("3. System");
         System.out.println("0. Quit");
         Scanner scanner = new Scanner(System.in);
-        scanner.nextLine();
+        try {
+            while (true) {
+                int choice;
+                if (scanner.hasNextInt()) {
+                    choice = scanner.nextInt();}
+                else{
+                    scanner.next();
+                    continue;
+                }
+                switch (choice) {
+                    case 1:
+                        System.out.println("1. Add");
+                        break;
+                    case 2:
+                        System.out.println("2. Delete");
+                        break;
+                    case 3:
+                        System.out.println("3. System");
+                        break;
+                    case 0:
+                        System.out.println("0. Quit");
+                        return; // You can use return to exit the loop and the method
+                    default:
+                        System.out.println("Wrong number");
+                }
+            }
+
+        } catch (InputMismatchException e) {
+            scanner.next(); // Consuming invalid input
+            System.out.println("Error, wrong input");
+        }
     }
 }
