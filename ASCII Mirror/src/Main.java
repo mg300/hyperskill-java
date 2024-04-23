7081fde5319e923919f96987846784c865946c12
Mateusz G <89965510+MateuszG2000@users.noreply.github.com>
2024-04-23 16:38:02 +0200

    ASCII Mirror stage 2/5

--- ASCII Mirror/src/Main.java
+++ ASCII Mirror/src/Main.java
@@ -1,12 +1,23 @@
+import java.io.*;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.print("                    _______ \n");
-        System.out.print("                   < hello >\n");
-        System.out.print("                    ------- \n");
-        System.out.print("            ^__^   /        \n");
-        System.out.print("    _______/(oo)  /         \n");
-        System.out.print("/\\/(       /(__)            \n");
-        System.out.print("   | w----||                \n");
-        System.out.print("   ||     ||                \n");
+        Scanner scanner = new Scanner(System.in);
+        String path = scanner.nextLine();
+        File file = new File(path);
+        System.out.println(path);
+        try{
+            BufferedReader buffer = new BufferedReader(new FileReader(file));
+            String str;
+            while ((str =buffer.readLine())!=null){
+                System.out.println(str);
+            }
+        }catch (FileNotFoundException e) {
+            System.err.println("File not found: " + e.getMessage());
+        } catch (IOException e) {
+            System.err.println("Input / output error: " + e.getMessage());
+        }
+
     }
 }
