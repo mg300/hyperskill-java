067b1da0851c683e0ce82c5209904cdf804a5e0f
Mateusz G <89965510+MateuszG2000@users.noreply.github.com>
2024-04-23 16:23:11 +0200

    ASCII Mirror stage 1/5

---
+++ ASCII Mirror/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("                    _______ \n");
+        System.out.print("                   < hello >\n");
+        System.out.print("                    ------- \n");
+        System.out.print("            ^__^   /        \n");
+        System.out.print("    _______/(oo)  /         \n");
+        System.out.print("/\\/(       /(__)            \n");
+        System.out.print("   | w----||                \n");
+        System.out.print("   ||     ||                \n");
+    }
+}
