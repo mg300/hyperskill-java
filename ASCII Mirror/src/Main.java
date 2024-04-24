f5d731daa3d882ebc88d3e63f859259e7981ac98
Mateusz G <89965510+MateuszG2000@users.noreply.github.com>
2024-04-24 16:53:29 +0200

    ASCII mirror stage 4/5

--- ASCII Mirror/src/Main.java
+++ ASCII Mirror/src/Main.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -7,17 +9,29 @@ public class Main {
         String path = scanner.nextLine();
         File file = new File(path);
         System.out.println(path);
+        List<String> lines = new ArrayList<>();
         try{
             BufferedReader buffer = new BufferedReader(new FileReader(file));
             String str;
             while ((str =buffer.readLine())!=null){
-                System.out.println(str);
+                lines.add(str);
             }
         }catch (FileNotFoundException e) {
             System.err.println("File not found: " + e.getMessage());
         } catch (IOException e) {
             System.err.println("Input / output error: " + e.getMessage());
         }
+         String maxLength = lines.stream()
+                .max(java.util.Comparator.comparingInt(String::length))
+                .orElse("");
 
+        for(String line : lines){
+            while (line.length()<maxLength.length()){
+                line = line+" ";
+            }
+            System.out.print(line);
+            System.out.print(" | ");
+            System.out.print(line+"\n");
+        }
     }
 }
